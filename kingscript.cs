8beb715c885afa09a6b3542d6ec49c94743cc7a3
mohini vaish <276mohinivaish@gmail.com>
2019-07-01 23:45:04 -0800

    push unity animations

---
+++ kingscript.cs
@@ -0,0 +1,39 @@
+﻿
+using TMPro;
+using System.Collections;
+using UnityEngine;
+
+
+
+public class kingscript : MonoBehaviour
+{
+
+    public TextMeshProUGUI textbox;
+    
+     void Start()
+    {
+      
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence()
+    {
+
+        yield return new WaitForSecondsRealtime(2);
+        Debug.Log("test");
+        textbox.text= "The land of Vitlandia awaits you my son! ";
+        yield return new WaitForSecondsRealtime(5);
+    textbox.text= "Make us proud, shower the people with all love and respect.";
+        yield return new WaitForSecondsRealtime(5);
+    textbox.text= "Son, I am old man now and wish to rest and spend my life now in peace! ";
+        yield return new WaitForSecondsRealtime(6);
+    textbox.text= "I wish that you may carry the legacy of our forefathers. But first you must prove your worth and embrace who you are! .";
+        yield return new WaitForSecondsRealtime(8);
+    textbox.text= "As the old saying [And the he who is himself needs not the world outside, finds fire within] ";
+        yield return new WaitForSecondsRealtime(7);
+
+    textbox.text = "           ";
+    }
+}
+
+       
---
+++ script2scene.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using System.Collections;
+using UnityEngine;
+
+
+
+public class script2scene : MonoBehaviour
+{
+
+    public TextMeshProUGUI textbox;
+
+    void Start()
+    {
+
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence()
+    {
+
+        yield return new WaitForSecondsRealtime(2);
+        Debug.Log("test1");
+        textbox.text = "COMMANDER IN CHIEF:             What is the weather there? ";
+        yield return new WaitForSecondsRealtime(5);
+        textbox.text = "SERVENT:                        Peaks will see the tempest, clouds assured thunder.Crop will not be good";
+        yield return new WaitForSecondsRealtime(5);
+        textbox.text = "COMMANDER IN CHIEF:              Ah! when the bamboo's will be ready?";
+        yield return new WaitForSecondsRealtime(8);
+        textbox.text = "SERVENT:                        Stars are falling, damp is the air, uprooted fall the trees, order stands nowhere! ";
+        yield return new WaitForSecondsRealtime(7);
+
+        textbox.text = "           ";
+    }
+}
